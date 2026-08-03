4464446fac0e6888491ab233cb0f6e2d491bff8c
NMichaelg <michaelnguyen8302@gmail.com>
2026-08-03 15:42:03 +0700

    Add shadcn/ui

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -59,7 +60,11 @@ export default function Home() {
             Documentation
           </a>
         </div>
+        <div className="flex min-h-svh items-center justify-center">
+          <Button>Click me</Button>
+        </div>
       </main>
     </div>
+    
   );
 }
